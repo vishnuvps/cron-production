0c829b4046e2c02d92b57e4fb41e7547542f23b6
vishnuvps <102712299+vishnuvps@users.noreply.github.com>
2023-01-11 18:50:45 +0530

    4 files modified

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -13,9 +13,28 @@ export class DashboardComponent implements OnInit {
 
   subscription: Subscription;
 
+  basicData: any;
+
+  basicOptions: any;
   constructor() {}
 
   ngOnInit() {
+    this.basicData = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'My First dataset',
+          backgroundColor: '#42A5F5',
+          data: [65, 59, 80, 81, 56, 55, 40],
+        },
+        {
+          label: 'My Second dataset',
+          backgroundColor: '#FFA726',
+          data: [28, 48, 40, 19, 86, 27, 90],
+        },
+      ],
+    };
+
     this.data = {
       datasets: [
         {
